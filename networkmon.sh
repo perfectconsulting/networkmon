a2ac854aaaab4476779ff6666da37f882048059c
Steven James <steven@perfectconsulting.co.uk>
2015-07-14 21:48:40 +0100

    Update networkmon.sh

--- networkmon.sh
+++ networkmon.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if ifconfig eth0 | grep -q "inet addr:" ;
+if ifconfig $1 | grep -q "inet addr:" ;
 then
         echo "$(date "+%m %d %Y %T") : Ethernet OK"
 else
         echo "$(date "+%m %d %Y %T") : Ethernet connection down! Attempting reconnection."
-        ifup --force eth0
+        ifup --force $1
         OUT=$? #save exit status of last command to decide what to do next
         if [ $OUT -eq 0 ] ; then
-                STATE=$(ifconfig eth0 | grep "inet addr:")
+                STATE=$(ifconfig $1 | grep "inet addr:")
                 echo "$(date "+%m %d %Y %T") : Network connection reset. Current state is" $STATE
         else
                 echo "$(date "+%m %d %Y %T") : Failed to reset ethernet connection"
